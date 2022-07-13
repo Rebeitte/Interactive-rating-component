91c1ee921bb08ff2381c6835ac2f76adfed32333
jesusrebeitte <jesusrebeitte@gmail.com>
2022-07-13 17:26:35 -0400

    button design

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,17 +1,22 @@
+//Selecting the DOM elements
 const button = document.querySelector('.send');
 let section = document.querySelector('.send_card');
-let ranks= document.querySelectorAll('.items')
+let ranks= document.querySelectorAll('.items');
+let close = document.querySelector('.close');
 
+//Iterate trought elements to give styles
 for( let i = 0; i < ranks.length; i++) {
     ranks[i].addEventListener('click', function selecting() {
         ranks[i].classList.toggle('number_selected')
         ranks[i].style.color = 'white';
-
-        console.log()
     })
 }
 
-button.addEventListener('click', function send() {
+// Send buttom 
+button.addEventListener('click', function send () {
     section.classList.add('index');  
 })
 
+close.addEventListener ("click", function close() {
+    section.classList.remove("index");
+})
